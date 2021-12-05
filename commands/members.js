b6f8649c8a36696ed906993144b93ac68ab23126
ClockClap <i.am.clockclap@gmail.com>
2021-12-05 22:08:21 +0900

    a

--- commands/members.js
+++ commands/members.js
@@ -4,6 +4,6 @@ module.exports = {
         description: "サーバーにいるメンバーの数を確認できます。",
     },
     async execute(client, interaction) {
-        interaction.reply({ content: `このサーバーには ${interaction.guild.memberCount} 人いるなあ、そうに決まってる。`});
+        interaction.reply({ content: `このサーバーには ${interaction.guild.member_count} 人いるなあ、そうに決まってる。`});
     }
 }
