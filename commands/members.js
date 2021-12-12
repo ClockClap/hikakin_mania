45cc29e907f1a5240af099758c5e51bc41264c0f
ClockClap <i.am.clockclap@gmail.com>
2021-12-12 14:56:25 +0900

    ?

--- commands/members.js
+++ commands/members.js
@@ -5,30 +5,15 @@ module.exports = {
     },
     async execute(client, interaction) {
         const g = interaction.guild;
-        let totalMembers = 0;
-        let people = 0;
-        let bots = 0;
-        g.map(m => {
-            totalMembers++;
-            m.bot ? bots++ : people++;
-        });
+        let totalMembers = g.memberCount;s
         await interaction.reply({ embeds: [{
                 author: {
                     name: interaction.member.tag,
                     icon_url: interaction.member.avatar_url
                 },
-                title: "全体の参加者",
+                title: "このサーバーの参加者",
                 description: `${totalMembers}`,
                 color: 0xFF22AA,
-                fields: [{
-                    name: "ボットを除いた人数",
-                    value: `${people}`,
-                    inline: true
-                },{
-                    name: "ボット",
-                    value: `${bots}`,
-                    inline: true
-                }],
                 timestamp: new Date(),
                 footer: {
                     icon_url: client.user.avatar_url,
