3752a59eb8899dced1cce0727364a4aed64425f6
ClockClap <i.am.clockclap@gmail.com>
2021-12-25 14:41:56 +0900

    遊び要素追加

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const client = new Client({ intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_PRESENCES], partials: ["CHANNEL"] });
 
@@ -26,7 +27,10 @@ client.on('ready', async () => {
     for (const commandName in commands) {
         data.push(commands[commandName].data);
     }
-    await client.guilds.cache.forEach(g => client.application.commands.set(data, g.id))
+    await client.guilds.cache.forEach(g => {
+        client.application.commands.set(data, g.id);
+        console.log(g.name);
+    })
     console.log(`Logged in as ${client.user.tag}`);
     const servers = client.guilds.cache.size;
     client.user.setActivity(`/help | ${servers} サーバー`, { type: "COMPETING" });
@@ -48,10 +52,10 @@ client.on("guildDelete", async guild => {
     client.user.setActivity(`/help | ${servers} サーバー`, { type: "COMPETING" });
 })
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
     if (message.author.bot) return;
     if (message.channel.type === 'DM') {
-        message.author.send({ content: '何かありましたか？\n' +
+        await message.author.send({ content: '何かありましたか？\n' +
                 'もし何かあった場合はサポートサーバーに入り、お問い合わせください。\n\n' +
                 '雑談サーバーでもあります。興味があればぜひ入ってくださいね！\n\n' +
                 '**☆ヒカマニ雑談サーバー☆**\n' +
@@ -59,8 +63,50 @@ client.on('messageCreate', message => {
                 'また、このボットを導入したい場合は下のURLをクリックしてください。\n' +
                 'https://discord.com/oauth2/authorize?client_id=908571927180890133&permissions=515466710592&scope=bot%20applications.commands'});
     } else {
-        if (message.content === '出たー出た！') {
-            message.reply({content: '<:deta:908720612824993862>', allowedMentions: {repliedUser: false}});
+        const c = message.content.toLowerCase();
+        if (c.includes('出たー出た') || c.includes('出た―出た')) {
+            await message.react('908720612824993862');
+        }
+        if(c.includes('出ない出ない')) {
+            await message.react('914519946426449970');
+            await message.react('908355751955800074');
+            await message.react('908733061141778483');
+        }
+        if(c.includes('<:tatanai:908733061141778483>')) {
+            await message.react('908733061141778483');
+        }
+        if(c.startsWith("お、勃った") || c.startsWith("お、◯った") || c.startsWith("お、立った")) {
+            await message.react('908723732380532796');
+        }
+        if(c === '<:nukeru:908733014748590090>') {
+            await message.react('908733014748590090');
+        }
+        if(c.startsWith('嘘だろ')) {
+            await message.react('914416280373710858');
+            await message.react('914415553194655774');
+        }
+        if(c.startsWith('<:dengeki:908347581174804521>')) {
+            await message.react('908719276221947994');
+            await message.react('908355042124394496');
+        }
+        if(c.includes('<:kani:908349208891912212>') || c.includes('レさの連鎖') || c.includes('カニキンが蚊に刺され') ||
+            c.includes('馬が埋まってる') || c.includes('ソーッスね') ||
+            c.includes('タモリがいた森') || c.includes('🦀') || c.includes(':crab:') ||
+            c.includes('肉が憎い') || c.includes('カレーは辛え') || c.includes('ストーブがストップ') ||
+            c.includes('運賃がうんち') || c.includes('棚から田中')) {
+            await message.react('908349208891912212');
+            await message.react('908347581174804521');
+        }
+        if(c.includes('うんこ') || c.includes('うんち') || c.includes('糞') ||
+            c.includes('クソ') || c.includes('crap') || c.includes('poop') ||
+            c.includes('ブリブリ排泄') || c.includes('fuck') || c.includes('unko') ||
+            c.includes('unti') || c.includes('unchi') ||
+            c.includes('ウンコ') || c.includes('ウンチ') || c.includes(':poop:') || c.includes('💩') ||
+            c.includes('汚ﾅｲ！') || c.includes('汚い') || c.includes('きたない')) {
+            await message.react('💩');
+        }
+        if(c.includes('<:biocola:908358390223364097>') || c.includes('アナ、ゥ')) {
+            await message.react('908358390223364097')
         }
     }
 })
@@ -78,4 +124,4 @@ client.on("interactionCreate", async interaction => {
     }
 });
 
-client.login();
+client.login('OTA4NTcxOTI3MTgwODkwMTMz.YY3riw.jxCpWBLtgy0eqFN2n0gjgqWDrg8');
